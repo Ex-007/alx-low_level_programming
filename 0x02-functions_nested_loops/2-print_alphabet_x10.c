110998dba7023808ba9c1d1f336b6e8884eb96a3
Ex-007 <agbebibidemi02@gmail.com>
2023-02-21 12:03:05 -0800

    2-print_alphabet_*10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
 /**
-* add - adds two integers and returns the result
-*
-* @a: int 1
-* @b: int 2
-*
-* Return: sum of integers
-*/
-int add(int a, int b)
+ * print_alphabet_x10 - this prints the lower cased alphabet 10 times.
+ *
+ * Return  0 (Success)
+ */
+void print_alphabet_x10(void)
 {
-	return (a + b);
+	int x10, a;
+
+	for (x10 = 0; x10 <= 9; x10++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(a);
+		}
+	_putchar('\n');
+	}
 }
+
