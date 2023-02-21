6e228888c4d495bada6aa941341aab3100efa514
Ex-007 <agbebibidemi02@gmail.com>
2023-02-21 11:05:39 -0800

    main.h and 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main-print -purchar and add a new line
+*i
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count = 0, size;
+
+	size = sizeof(str) / sizeof(int);
+	do {
+		_putchar(str[count]);
+		count++;
+	} while (count < size);
+	_putchar('\n');
+	return (0);
